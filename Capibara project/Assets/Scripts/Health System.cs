fef2aed4dfa5b92586558b8d95c3f4a5ccbeb31c
Matteo Mosconi <matteo.mosconi.1999@gmail.com>
2025-01-25 23:47:12 +0100

    Oxigen Loss

--- Capibara project/Assets/Scripts/Health System.cs
+++ Capibara project/Assets/Scripts/Health System.cs
@@ -7,11 +7,23 @@ public class HealthSystem : MonoBehaviour
     private float currentHealth;          // Salute corrente
     public Slider healthBar;              // Barra della vita da assegnare nell'Inspector
     public GameObject deathEffect;        // Oggetto che appare quando il personaggio muore (opzionale)
+    public float oxygenLossRate = 1f;     // Velocità di diminuzione della salute (simulando la perdita di ossigeno)
 
     void Start()
     {
         currentHealth = maxHealth;        // Inizializziamo la salute corrente con la salute massima
         UpdateHealthBar();                 // Inizializziamo la barra della vita
+        StartOxygenLoss();
+    }
+    private void StartOxygenLoss()
+    {
+        InvokeRepeating("OxygenLoss", 1f, 1f); // Chiama la funzione OxygenLoss ogni secondo
+    }
+
+    // Funzione che riduce gradualmente la salute come la perdita di ossigeno
+    private void OxygenLoss()
+    {
+        TakeDamage(oxygenLossRate);  // Riduce la salute di una certa quantità ogni volta che viene chiamato
     }
 
     // Funzione per danneggiare il personaggio
