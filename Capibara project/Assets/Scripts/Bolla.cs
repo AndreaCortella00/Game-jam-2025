1713806c6fe5971c5cbded5eebc51844c1084d5c
Matteo Mosconi <matteo.mosconi.1999@gmail.com>
2025-01-26 12:32:24 +0100

    Bolla Heal

---
+++ Capibara project/Assets/Scripts/Bolla.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bolla : MonoBehaviour
+{
+    
+    public HealthSystem healthSystem;
+    // Quando il giocatore entra in collisione con la moneta
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))  // Controlla se l'oggetto che ha fatto la collisione è il giocatore
+        {
+            // Aumenta vita
+            healthSystem.Heal(10f);
+
+            // Distrugge la moneta (opzionale: puoi farla nascondere o creare un'animazione)
+            Destroy(gameObject);
+        }
+    }
+}
+
